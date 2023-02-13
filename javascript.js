fe4c0e4a70813ac567cff412321836642b1b8e64
SenseiSimms <luciddreamstates@gmail.com>
2023-02-13 01:40:29 -0600

    2/13/23 refactor JS for grid creation

--- javascript.js
+++ javascript.js
@@ -5,19 +5,27 @@ const newGridBtn = document.querySelector("#newGridBtn");
 
 
 
-let buildGrid = function () {
+
+let buildGrid = function buildGrid() {
+    let z = prompt("How many squares per side?");
+    let gridArea = z * z;
    
-    for (let i = 0; i < 16*16; i++)
+
+    for (let i = 1; i <= gridArea; i++)
     {
-const newDiv = document.createElement("div");
+       
+        const newDiv = document.createElement("div");
+
+
 newDiv.className = "newDiv";
 newDiv.style.outline = "solid black 1px";
-newDiv.style.height = "20px";
-newDiv.style.width = "20px";
+newDiv.style.gridTemplateColumns = `repeat(${z}}, 1fr)`;
+newDiv.style.gridTemplateRows = `repeat(${z}, 1fr)`;
+newDiv.style.width = "25px";
+newDiv.style.height = "25px";
 container.appendChild(newDiv);}
-
-
 };
+
 
 
 
