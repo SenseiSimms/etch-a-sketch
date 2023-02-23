1cffe0ed3b3b68e05866d7c25ed54e38357f16fa
SenseiSimms <luciddreamstates@gmail.com>
2023-02-23 08:09:19 -0600

    2/23/2023 add more functions to JavaScript file.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,38 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 const newGridBtn = document.querySelector("#newGridBtn");
 
 
-
-
-
-
-let buildGrid = function buildGrid() {
-    let z = prompt("How many squares per side?");
-    let gridArea = z * z;
+//A function that sets a range of how big or small the grid can be as to not crash the site.
+function sizeRange(input) {
    
+    if (input >= 2 && input <= 100) {
+        newGrid;
+    } else {
+        console.log("Too many squares");
+    }
+}
 
-    for (let i = 1; i <= gridArea; i++)
+//When button is clicked, a pop-up appears asking for the size of grid to be made.
+function newGrid() 
+{
+    let squares = prompt("How many squares on each side?");
+   const gridArea = squares * squares;
+   sizeRange(squares);
+    
+
+    for (let i = 0; i <= gridArea; i++)
+
     {
-       
+        container.style.gridTemplateColumns = `repeat(${squares} , 1fr)`;
+        container.style.gridTemplateRows = `repeat(${squares} , 1fr)`;
         const newDiv = document.createElement("div");
+        newDiv.className = "newDiv";
+        newDiv.style.outline = "solid grey 1px";
 
+        container.appendChild(newDiv);
+}
 
-newDiv.className = "newDiv";
-newDiv.style.outline = "solid black 1px";
-newDiv.style.gridTemplateColumns = `repeat(${z}}, 1fr)`;
-newDiv.style.gridTemplateRows = `repeat(${z}, 1fr)`;
-newDiv.style.width = "25px";
-newDiv.style.height = "25px";
-container.appendChild(newDiv);}
-};
+;}
 
-
-
-
-newGridBtn.addEventListener("click", buildGrid);
+newGridBtn.addEventListener("click", newGrid);
 
