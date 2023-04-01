e61b7eb09dc669ca3e3ad4b8758c895be44c8602
SenseiSimms <luciddreamstates@gmail.com>
2023-04-01 15:25:24 -0500

    4/1/2023 add more JS to buttons

--- javascript.js
+++ javascript.js
@@ -1,38 +1,46 @@
 const container = document.querySelector(".container");
+
 const newGridBtn = document.querySelector("#newGridBtn");
 
+const newDiv = document.createElement("div");
+newDiv.className = "newDiv";
 
-//A function that sets a range of how big or small the grid can be as to not crash the site.
-function sizeRange(input) {
+const blackColorBtn = document.querySelector(".newDiv");
+newDiv.style.backgroundColor = "black";
+
    
-    if (input >= 2 && input <= 100) {
-        newGrid;
-    } else {
-        console.log("Too many squares");
-    }
-}
 
 //When button is clicked, a pop-up appears asking for the size of grid to be made.
-function newGrid() 
-{
-    let squares = prompt("How many squares on each side?");
-   const gridArea = squares * squares;
-   sizeRange(squares);
+
+    function newGrid() {
+        let squares = prompt("How many squares on each side?");
+        const gridArea = squares * squares;
+    
+        if (sizeRange(squares)) {
+        for (let i = 0; i < gridArea; i++) {
+            container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+            
+            newDiv.style.outline = "solid grey 1px";
+    
+            container.appendChild(newDiv);
+        }
+        }
+    }
+
+//A function that sets a range of how big or small the grid can be as to not crash the site.
+
+    function sizeRange(input) {
+        if (input >= 2 && input <= 100) {
+        return true;
+        } else {
+        alert(`${input} is an invalid size, squares can only be between 2 and 100.`);
+        return false;
+        }
+    }
     
 
-    for (let i = 0; i <= gridArea; i++)
 
-    {
-        container.style.gridTemplateColumns = `repeat(${squares} , 1fr)`;
-        container.style.gridTemplateRows = `repeat(${squares} , 1fr)`;
-        const newDiv = document.createElement("div");
-        newDiv.className = "newDiv";
-        newDiv.style.outline = "solid grey 1px";
-
-        container.appendChild(newDiv);
-}
-
-;}
 
 newGridBtn.addEventListener("click", newGrid);
-
+newDiv.addEventListener("mouseover", blackColorBtn);
